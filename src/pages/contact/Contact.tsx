6c5d4074fc331b13d39ff231b200e15d5afc0cc8
Geno Ureta <Geno Ureta>
2024-07-11 15:34:20 +0800

    first commit

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,5 +1,6 @@
 import ScrollAnimation from "react-animate-on-scroll";
 import { AiOutlineMail } from "react-icons/ai";
+import { FaPhone } from "react-icons/fa6";
 
 export default function Contact() {
   return (
@@ -9,7 +10,7 @@ export default function Contact() {
       </ScrollAnimation>
 
       <ScrollAnimation className="flex items-center justify-center"  animateIn='slideInUp' delay={200}>
-      <p>
+      <p className="text1">
         I’d love to hear from you! Whether you have a project in mind, need a website overhaul,
         or just want to say hello, feel free to reach out.
       </p>
@@ -20,6 +21,7 @@ export default function Contact() {
         <button className="contactBtn">
           <AiOutlineMail/>Email Me</button>
       </a>
+      <p className="text2"><FaPhone /> +63 947 377 8502</p>
       </ScrollAnimation>
 
       <span>
